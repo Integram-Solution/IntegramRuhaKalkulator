2c6fefd78c03791c394f185ac97a14dadfd63e76
Gabi00720 <abcsi.o007@gmail.com>
2024-11-13 13:22:02 +0100

    delete backend

--- backend/test_db_connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-try {
-    $pdo = new PDO('mysql:host=backend-mysql-1;dbname=laravel', 'sail', 'password');
-    echo "Sikeres MySQL kapcsolat!";
-} catch (PDOException $e) {
-    echo "Kapcsolódási hiba: " . $e->getMessage();
-}
